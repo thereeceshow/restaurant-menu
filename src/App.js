32b76f67b65dc800cd0a28d59999a99136b5ff87
Reece Walter <reece@reecewalter.com>
2021-03-26 10:33:32 -0400

    working home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-3 splash">
+      <h1 className="text-end">Welcome to Bourbon-Dynasty</h1>
+      <h3 className="text-end">Lexington's Finest Bourbon Dive</h3>
+      <h6 className="text-end">Haute Whiskey and Cheap Food</h6>
     </div>
   );
 }
